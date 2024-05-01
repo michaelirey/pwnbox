e3da9d6b82aa0629d5bf58817ec9582db40bd74d
Michael Irey <michael.irey@gmail.com>
2024-05-01 13:12:20 -0700

    Adds more stuff to system report

--- system_report.sh
+++ system_report.sh
@@ -12,11 +12,25 @@ public_ip=$(curl -s https://ifconfig.me)
 # Hostname
 hostname=$(hostname)
 
-# Available package managers
+# System Architecture
+architecture=$(uname -m)
+
+# CPU and Memory Information
+cpu_info=$(lscpu | grep "Model name:" | cut -d ':' -f2 | xargs)
+cpu_cores=$(lscpu | grep "^CPU(s):" | cut -d ':' -f2 | xargs)
+total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2 $3}')
+
+# Disk Space
+disk_space=$(df -h / | awk 'NR==2 {print $4}')
+
+# Network Interfaces
+network_interfaces=$(ip link show | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | xargs)
+
+# Available Package Managers
 package_managers=$(echo $(which apt yum dnf 2>/dev/null))
 
 # List of scripting language interpreters to check
-languages=("python" "ruby" "perl" "php" "node")
+languages=("python" "ruby" "perl" "php" "node" "lua")
 
 # Function to get version information
 get_version() {
@@ -34,6 +48,11 @@ echo "OS and Version: $os_info"
 echo "Local IP: $local_ip"
 echo "Public IP: $public_ip"
 echo "Hostname: $hostname"
+echo "System Architecture: $architecture"
+echo "CPU Info: $cpu_info, Cores: $cpu_cores"
+echo "Total Memory: $total_mem"
+echo "Available Disk Space: $disk_space"
+echo "Network Interfaces: $network_interfaces"
 echo "Available Package Managers: $package_managers"
 echo "Programming Languages and Details:"
 for lang in "${languages[@]}"; do
