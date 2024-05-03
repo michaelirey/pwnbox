bd628f69d92a4d8a6c402c72ff945dcdf175c97a
Michael Irey <michael.irey@gmail.com>
2024-05-03 10:17:33 -0700

    Adds scripts to root path

--- start_webserver.sh
+++ start_webserver.sh
@@ -2,4 +2,7 @@ NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0]
 echo -e "Set the proxy:\n"
 echo "export NGROK_PROXY='${NGROK_URL}'"
 echo -e "\n"
-cd ~/pwnbox && sudo ruby webserver.rb
+cd ~/pwnbox
+sudo cp -a port_scanner.sh /root
+sudo cp -a system_report.sh /root
+sudo ruby webserver.rb
