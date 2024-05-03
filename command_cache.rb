f7abf7199808593a35a1aa6d9022c501c129f7b2
Michael Irey <michael.irey@gmail.com>
2024-05-03 00:00:43 -0700

    WIP

--- command_cache.rb
+++ command_cache.rb
@@ -1,3 +1,5 @@
+class CacheReadError < StandardError; end
+
 class CommandCache
   def initialize(cache_dir = "command_cache")
     @cache_dir = cache_dir
@@ -7,10 +9,9 @@ class CommandCache
   def md5_for(command)
     Digest::MD5.hexdigest(command)
   end
-  
+
   def get(command)
-    md5_hash = Digest::MD5.hexdigest(command)
-    cache_path = File.join(@cache_dir, md5_hash)
+    cache_path = cache_path_for(command)
     if File.exist?(cache_path)
       cached_data = File.read(cache_path).split("\n\n", 2)
       if cached_data.size == 2
@@ -18,8 +19,7 @@ class CommandCache
           cached_response_json = Base64.decode64(cached_data[1])
           JSON.parse(cached_response_json)
         rescue JSON::ParserError => e
-          @logger.error "Failed to parse JSON from cache: #{e.message}"
-          nil
+          raise CacheReadError.new("Failed to parse JSON from cache: #{e.message}")
         end
       end
     else
@@ -28,9 +28,14 @@ class CommandCache
   end
 
   def set(command, response_body)
-    md5_hash = Digest::MD5.hexdigest(command)
-    cache_path = File.join(@cache_dir, md5_hash)
+    cache_path = cache_path_for(command)
     cache_content = Base64.encode64(command) + "\n" + Base64.encode64(response_body)
     File.write(cache_path, cache_content)
+  end
+
+  private
+
+  def cache_path_for(command)
+    File.join(@cache_dir, md5_for(command))
   end
 end
